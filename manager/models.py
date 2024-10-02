ee4e0e406d9e0746f70ed62c9b068208abcaf543
Anton Korinchuk <antonkorinchuk@gmail.com>
2024-10-02 17:11:16 +0300

    Add models.py

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class People(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="peoples", null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
