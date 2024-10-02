f48aa78e043a0b0de18f4ae7999068b96da8bb68
Anton Korinchuk <antonkorinchuk@gmail.com>
2024-10-02 17:39:00 +0300

    Add views

--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from models import Team, People
+from serializers import TeamSerializer, PeopleSerializer
+
+
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+
+class PeopleViewSet(viewsets.ModelViewSet):
+    queryset = People.objects.all()
+    serializer_class = PeopleSerializer
