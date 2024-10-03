6adb5868e1cd00bf1cb408698f67fe9a45d6700c
Anton Korinchuk <antonkorinchuk@gmail.com>
2024-10-03 17:10:09 +0300

    Fix N+1

--- manager/views.py
+++ manager/views.py
@@ -5,10 +5,10 @@ from manager.serializers import TeamSerializer, PeopleSerializer
 
 
 class TeamViewSet(viewsets.ModelViewSet):
-    queryset = Team.objects.all()
+    queryset = Team.objects.prefetch_related("peoples")
     serializer_class = TeamSerializer
 
 
 class PeopleViewSet(viewsets.ModelViewSet):
-    queryset = People.objects.all()
+    queryset = People.objects.select_related("team")
     serializer_class = PeopleSerializer
